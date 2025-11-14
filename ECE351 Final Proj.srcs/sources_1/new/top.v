dd95dce1d30aff69f319a2a80f3d264dd2a89211
DavyjoneCode <davidinpy@protonmail.com>
2025-11-14 08:51:03 -0500

    settup project

---
+++ ECE351 Final Proj.srcs/sources_1/new/top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/14/2025 08:42:26 AM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+
+    );
+endmodule
